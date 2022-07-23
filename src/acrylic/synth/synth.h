ef09a86feea691b98694ff9de8d6418d83e010db
Cillié Malan <cilliemalan@gmail.com>
2022-07-23 13:40:31 +0200

    [WIP] synth stuff

--- src/acrylic/synth/synth.h
+++ src/acrylic/synth/synth.h
@@ -1,5 +1,14 @@
 #pragma once
 
 #define SAMPLE_RATE 48000
-#define OO_SAMPLE_RATE (1.0f / 48000.0f)
+#define OO_SAMPLE_RATE 2.083333333333333e-5f // (1.0f / 48000.0f)
 
+#include "envelope.h"
+#include "oscillator.h"
+#include "filter.h"
+#include "util.h"
+
+struct acrylic_synth {
+    acrylic_oscillator_t oscillator1;
+    acrylic_oscillator_t oscillator2;
+} acrylic_synth_t;
