3df4123e82f6a00fa9e44322ea95936454f5efb2
Cillié Malan <cilliemalan@gmail.com>
2022-08-02 21:11:55 +0200

    acrylic_filter_lowpass declaration

--- src/acrylic/synth/filter.h
+++ src/acrylic/synth/filter.h
@@ -11,4 +11,5 @@ typedef struct filter
 	float py0, py1, py2;
 } filter_t;
 
+void acrylic_filter_lowpass(float frequency, float q, filter_t *filter);
 void acrylic_filter_process(float* buffer, unsigned int amt, filter_t *filter);
